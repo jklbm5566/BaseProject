bdfc1fa85dd24c8f236d61ae149953c1dacfd688
NN0414 <32450739+NN0414@users.noreply.github.com>
2018-01-12 20:40:36 +0800

    Calculate V2

--- src/main/java/Calculate/src/CalculatePack/Calculate.java
+++ src/main/java/Calculate/src/CalculatePack/Calculate.java
@@ -44,7 +44,7 @@ public class Calculate {
 
 	private void TOKEN() {
 		int i=0;
-		part = new String[7 * howDeep()];
+		part = new String[8 * howDeep()];
 		while(FileInput.hasNext()) {
 			StringTokenizer st = new StringTokenizer(FileInput.nextLine()," ");
 			while(st.hasMoreTokens()) {
@@ -58,16 +58,27 @@ public class Calculate {
 		double du=0;
 		int[] time = new int[3];
 		for(int i=part.length-1;i>=0;i--) {
+			int month=0;
 			/* Year  ||month ||day  */
-			if(i%7==6||i%7==2||i%7==3) {
-				if(i>=7) {
-					if(i%7==6) {
+			if(i%8==7||i%8==3||i%8==4) {
+				if(i>=8) {
+					if(i%8==7) {
 						du=(365*24);
 						if(isLeapYear(part[i])) du+=24;
 					}
-					else if(i%7==2) {
-						int month=0;
+					else if(i%8==3) {
 						if(part[i].equals("Jan")) month=1;
+						else if(part[i].equals("Feb")) month=2;
+						else if(part[i].equals("Mar")) month=3;
+						else if(part[i].equals("Apr")) month=4;
+						else if(part[i].equals("May")) month=5;
+						else if(part[i].equals("Jun")) month=6;
+						else if(part[i].equals("Jul")) month=7;
+						else if(part[i].equals("Aug")) month=8;
+						else if(part[i].equals("Sep")) month=9;
+						else if(part[i].equals("Oct")) month=10;
+						else if(part[i].equals("Nov")) month=11;
+						else if(part[i].equals("Dec")) month=12;
 						for(int mon=1;mon<=month;mon++) {
 							if(mon==1||mon==3||mon==5||mon==7||mon==8||mon==10||mon==12) du+=(31*24);
 							else if(mon==4||mon==6||mon==9||mon==11) du+=(30*24);
@@ -75,20 +86,30 @@ public class Calculate {
 							else if(mon==2 && !isLeapYear(part[i+4])) du+=(28*24); 
 						}
 					}
-					else if(i%7==3) {
+					else if(i%8==4) {
 						int day = 0;
 						day = Integer.parseInt(part[i]);
 						for(int da=1;da<day+1;da++) du+=24;
 					}
 				}
 				else {
-					if(i%7==6) {
+					if(i%8==7) {
 						du-=(365*24);
 						if(isLeapYear(part[i])) du-=24;
 					}
-					else if(i%7==2) {
-						int month=0;
+					else if(i%8==3) {
 						if(part[i].equals("Jan")) month=1;
+						else if(part[i].equals("Feb")) month=2;
+						else if(part[i].equals("Mar")) month=3;
+						else if(part[i].equals("Apr")) month=4;
+						else if(part[i].equals("May")) month=5;
+						else if(part[i].equals("Jun")) month=6;
+						else if(part[i].equals("Jul")) month=7;
+						else if(part[i].equals("Aug")) month=8;
+						else if(part[i].equals("Sep")) month=9;
+						else if(part[i].equals("Oct")) month=10;
+						else if(part[i].equals("Nov")) month=11;
+						else if(part[i].equals("Dec")) month=12;
 						for(int mon=1;mon<=month;mon++) {
 							if(mon==1||mon==3||mon==5||mon==7||mon==8||mon==10||mon==12) du-=(31*24);
 							else if(mon==4||mon==6||mon==9||mon==11) du-=(30*24);
@@ -96,7 +117,7 @@ public class Calculate {
 							else if(mon==2 && !isLeapYear(part[i+4])) du-=(28*24); 
 						}
 					}
-					else if(i%7==3) {
+					else if(i%8==4) {
 						int day = 0;
 						day = Integer.parseInt(part[i]);
 						for(int da=1;da<day+1;da++) du-=24;
@@ -142,8 +163,8 @@ public class Calculate {
 		return MAX <= w*duration();
 	}
 	
-//	public static void main(String[] args) {
-//		Calculate test = new Calculate();
-//		System.out.println(test.needConserve());
-//	}
+	public static void main(String[] args) {
+		Calculate test = new Calculate();
+		System.out.println(test.needConserve());
+	}
 }
